307022971e9180e5bcf93147dd96d999c652849c
zkhk <zkhk@Mac-mini.local>
2016-12-01 17:52:51 +0800

    添加网络请求类

---
+++ app/src/main/java/android/bignerdranch/com/photogallery/FlickrFetchr.java
@@ -0,0 +1,44 @@
+package android.bignerdranch.com.photogallery;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+/**
+ * Created by zkhk on 2016/12/1.
+ */
+
+// 用于网络请求连接的Viwe
+public class FlickrFetchr {
+
+    public byte[] getUrlBytes(String urlSpec) throws IOException {
+        URL url = new URL(urlSpec);
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new IOException(connection.getResponseMessage() + ": with" + urlSpec);
+            }
+            int byteRead = 0;
+            byte[] buffer = new byte[1024];
+            while ((byteRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, byteRead);
+            }
+            out.close();
+            return out.toByteArray();
+
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+    public String getUrlString(String urlSpec) throws IOException {
+        return new String(getUrlBytes(urlSpec));
+    }
+
+}
